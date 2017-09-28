d10c8a7a4bf6fe5f751d77164b31e33f74e9a3fe
Imre Mocsari <imremocsari1@yahoo.co.uk>
2017-09-28 16:25:33 +0100

    video 15

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
-
+import Fish from './Fish';
 class App extends React.Component {
   constructor() {
     super();
@@ -31,6 +31,11 @@ class App extends React.Component {
     <div className='catch-of-the-day'>
       <div className='menu'>
         <Header tagline="Fresh food market"/>
+        <ul className='list-of-fishes'>
+          { Object
+            .keys(this.state.fishes)
+            .map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+        </ul>
       </div>
       <Order/>
       <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
